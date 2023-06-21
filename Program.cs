bbc829e52d37c4612fe46b95ce66f5bdf9c81854
Joniclei <jonicleibernardojunior@gmail.com>
2023-06-20 23:32:54 -0300

    novo

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PessoasMain
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int op;
+
+      do{
+      Console.WriteLine("1-Crie um ser humaninho");
+      Console.WriteLine("2-Liste");
+      Console.WriteLine("3-TroKis");
+      Console.WriteLine("4-Deletis ");
+      Console.WriteLine("5-Sair");
+      op = Convert.ToInt32(Console.ReadLine());
+      switch(op){
+        case 1:{
+
+          break;
+        }
+        case 2:{
+
+          break;
+        }
+        case 3:{
+
+          break;
+        }
+        case 4:{
+         
+          break;
+        }
+      }
+
+      }while(op!= 5);
+
+    }
+  }
+}
